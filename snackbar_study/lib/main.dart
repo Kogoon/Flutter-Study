15df8d2d5d02efeacdd4722e5317c629d51884b6
Kogoon <gojs712@gmail.com>
2021-03-10 22:35:00 +0900

    Study Flutter's SnackBar

---
+++ snackbar_study/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Snack Bar',
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack Bar Study'),
+        centerTitle: true,
+      ),
+      body: Builder(
+        builder: (BuildContext ctx) {
+          return Center(
+            child: FlatButton(
+              child: Text(
+                'Show me',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.red,
+              onPressed: () {
+                Scaffold.of(ctx).showSnackBar(SnackBar(
+                  content: Text('Hellow'),
+                  backgroundColor: Colors.grey,
+                ));
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
